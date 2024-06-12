6f4728439137e18d62779f00e5520d60affb0cab
Balya Elfata <balyaelfata@yahoo.com>
2024-06-12 14:08:39 +0700

    Initial Commit

---
+++ Shuttle of Destiny/Shuttle_of_DestinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shuttle_of_DestinyApp.swift
+//  Shuttle of Destiny
+//
+//  Created by Balya Elfata on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Shuttle_of_DestinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
