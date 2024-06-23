64dc52df857d13e0da3aa373104a243538c9ee61
Balya Elfata <balyaelfata@yahoo.com>
2024-06-24 01:33:39 +0700

    Added background assets and character sprites during conversation to prologue view, added prologue navigation to eventview

--- Shuttle of Destiny/View/PrologView.swift
+++ Shuttle of Destiny/View/PrologView.swift
@@ -4,9 +4,15 @@ import Foundation
 struct PrologView: View {
     @State var conversationIndex = 0
     var conversationList: [String] = []
+    var alexConversation: [String] = []
+    var dadConversation: [String] = []
+    var miaConversation: [String] = []
+    var coachConversation: [String] = []
+    var doctorConversation: [String] = []
+    let blackScreen = "  "
+    let noConversation = " "
     let conversation1Alex = "Dad, look! I got it!"
     let conversation2Dad = "I'm so proud of you, my son"
-    let noConversation = " "
     let conversation3Dad = "You know, Alex, every time I see you on that court, it's like watching a part of myself out there."
     let conversation4Alex = "I learned from the best, Dad. You've always been my biggest inspiration."
     let conversation5Dad = "I just wish I could've been the world champion. But you... you have the chance to achieve what I couldn't."
@@ -33,88 +39,241 @@ struct PrologView: View {
     let conversation26Mia = "Anytime, Alex. Get some rest. Tomorrow’s another big day."
     let conversation27Alex = "I won’t let you down, Dad. I’ll make your dream a reality."
     
-    @State var fadeToBlack = false
+    @State var sceneNumber = 1
     
-    @State private var animatedText: String = ""
+    @State private var showDailyPlayView = false
+    
+//    @State private var animatedText: String = ""
     
     init() {
         // Conversation list
         conversationList = [
+            noConversation,
             conversation1Alex,
             conversation2Dad,
-            noConversation,
+            blackScreen,
             conversation3Dad,
             conversation4Alex,
             conversation5Dad,
             conversation6Alex,
-            noConversation,
+            blackScreen,
             conversation7Coach,
             conversation8Alex,
             conversation9Coach,
             conversation10Alex,
-            noConversation,
+            blackScreen,
             conversation11Mia,
             conversation12Alex,
             conversation13Mia,
             conversation14Alex,
             conversation15Mia,
-            noConversation,
+            blackScreen,
             conversation16Doctor,
             conversation17Alex,
             conversation18Doctor,
             conversation19John,
             conversation20Alex,
             conversation21Dad,
-            noConversation,
+            blackScreen,
             conversation22Mia,
             conversation23Alex,
             conversation24Mia,
             conversation25Alex,
             conversation26Mia,
-            noConversation,
+            blackScreen,
             conversation27Alex,
+        ]
+    
+        alexConversation = [
+            conversation1Alex,
+            conversation4Alex,
+            conversation6Alex,
+            conversation8Alex,
+            conversation10Alex,
+            conversation12Alex,
+            conversation14Alex,
+            conversation17Alex,
+            conversation20Alex,
+            conversation23Alex,
+            conversation25Alex,
+            conversation27Alex,
+        ]
+        
+        dadConversation = [
+            conversation2Dad,
+            conversation3Dad,
+            conversation5Dad,
+            conversation21Dad,
+        ]
+        
+        miaConversation = [
+            conversation11Mia,
+            conversation13Mia,
+            conversation15Mia,
+            conversation22Mia,
+            conversation24Mia,
+            conversation26Mia,
+        ]
+        
+        coachConversation = [
+            conversation7Coach,
+            conversation9Coach,
+        ]
+        
+        doctorConversation = [
+            conversation16Doctor,
+            conversation18Doctor,
         ]
     }
 
     var body: some View {
-        ZStack {
-            VStack (){
-                Image("park")
-                    .resizable()
-                    .ignoresSafeArea(.all)
-                
-                Text(animatedText)
-                    .font(.title)
-                    .padding()
-            }
-            Color.black
-                .ignoresSafeArea(.all)
-                .opacity(animatedText == " " ? 1 : 0)
-                .animation(.easeInOut(duration: 1.0))
-        }
-        .onTapGesture {
-            if conversationIndex < conversationList.count{
-                animateText(text: conversationList[conversationIndex])
-                conversationIndex+=1
-            }
-            else {
-                animateText(text: "Conversation is Completed")
+        if showDailyPlayView {
+            EventView()
+        } else {
+            GeometryReader { geo in
+                ZStack {
+                    switch sceneNumber {
+                    case 1:
+                        Image("prolog1")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 2:
+                        Image("prolog2")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 3:
+                        Image("prolog3")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 4:
+                        Image("prolog4")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 5:
+                        Image("prolog5")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 6:
+                        Image("prolog6")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 7:
+                        Image("prolog7")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 8:
+                        Image("prolog8")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 9:
+                        Image("prolog9")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 10:
+                        Image("prolog10")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 11:
+                        Image("prolog11")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    case 12:
+                        Color.black
+                            .ignoresSafeArea(.all)
+                    default:
+                        Image("park")
+                            .resizable()
+                            .ignoresSafeArea(.all)
+                    }
+                    if alexConversation.contains(conversationList[conversationIndex]) {
+                        Image("MC Sprite")
+                            .resizable()
+                            .frame(width: 222, height: 231)
+                            .position(x: 80, y:200)
+                            .opacity(0.9)
+                    }
+                    else if dadConversation.contains(conversationList[conversationIndex]) {
+                        Image("Dad Sprite")
+                            .resizable()
+                            .frame(width: 240, height: 240)
+                            .position(x: 680, y:220)
+                            .opacity(0.9)
+                    }
+                    else if miaConversation.contains(conversationList[conversationIndex]) {
+                        Image("Mia Sprite")
+                            .resizable()
+                            .frame(width: 240, height: 240)
+                            .position(x: 680, y:220)
+                            .opacity(0.9)
+                    }
+                    VStack (){
+                        Spacer()
+                        ZStack {
+                            Image("Chat Box")
+                                .opacity(conversationList[conversationIndex] == blackScreen || conversationList[conversationIndex] == noConversation ? 0 : 0.9)
+                                .overlay(alignment: .leading){
+                                    Text(conversationList[conversationIndex])
+                                        .font(.custom(Constants.vtFont, size: geo.size.width * Constants.smallTextSize))
+                                        .padding(.horizontal, 50)
+                                        .padding(.vertical, 15)
+                                }
+                        }
+                    }
+                    Color.black
+                        .ignoresSafeArea(.all)
+                        .opacity(conversationList[conversationIndex] == blackScreen ? 1 : 0)
+                        .animation(.easeInOut(duration: 1.0))
+                }
+                .onTapGesture {
+                    nextConversation()
+                    switch conversationIndex {
+                    case 0:
+                        sceneNumber = 1
+                    case 1:
+                        sceneNumber = 2
+                    case 4:
+                        sceneNumber = 3
+                    case 5:
+                        sceneNumber = 4
+                    case 9:
+                        sceneNumber = 7
+                    case 14:
+                        sceneNumber = 9
+                    case 20:
+                        sceneNumber = 10
+                    case 27:
+                        sceneNumber = 11
+                    case 33:
+                        sceneNumber = 12
+                    default:
+                        return
+                    }
+                }
+                .onAppear {
+                    Helper.sharedHelper.playPrologueMusic()
+                }
             }
         }
     }
 
-    func animateText(text: String) {
-        animatedText = ""
-        for (index, character) in text.enumerated() {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.11) {
-                animatedText.append(character)
-                UIImpactFeedbackGenerator(style: .light).impactOccurred()
-            }
-        }
-    }
+//    func animateText(text: String) {
+//        animatedText = ""
+//        for (index, character) in text.enumerated() {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.11) {
+//                animatedText.append(character)
+//                UIImpactFeedbackGenerator(style: .light).impactOccurred()
+//            }
+//        }
+//    }
     
     func nextConversation() {
-        conversationIndex+=1
+        if conversationIndex < conversationList.count-1{
+//            animateText(text: conversationList[conversationIndex])
+            conversationIndex+=1
+        }
+        else {
+            showDailyPlayView = true
+        }
     }
 }
 
