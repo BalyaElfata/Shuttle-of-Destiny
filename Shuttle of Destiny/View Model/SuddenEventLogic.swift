e01c35a53106b325dab9cb80f6a5327a9832d0a8
Balya Elfata <balyaelfata@yahoo.com>
2024-06-24 01:30:31 +0700

    Commented conflict files

--- Shuttle of Destiny/View Model/SuddenEventLogic.swift
+++ Shuttle of Destiny/View Model/SuddenEventLogic.swift
@@ -1,38 +1,38 @@
+////
+////  SuddenEventLogic.swift
+////  sudden_event
+////
+////  Created by Geraldo Pannanda Lutan on 12/06/24.
+////
 //
-//  SuddenEventLogic.swift
-//  sudden_event
+//import Foundation
+//import SwiftUI
+//import SwiftData
 //
-//  Created by Geraldo Pannanda Lutan on 12/06/24.
+//// Assuming DataItem is defined elsewhere in your project
+//var pointModels: DataItem = DataItem() // SwiftData Variable
 //
-
-import Foundation
-import SwiftUI
-import SwiftData
-
-// Assuming DataItem is defined elsewhere in your project
-var pointModels: DataItem = DataItem() // SwiftData Variable
-
-let event = [
-    Gameplay(SuddenEventTitles: ["Sudden Title"], SuddenEventDescs: ["Sudden Event Description"], SuddenPointPluses: [1], SuddenPointMinuses: [1], SuddenPointPlusesOther: [1], SuddenPointMinusesOther: [1],SuddenEventType: 1, id: 0, Used: false)
-]
-
-var gameplay: Gameplay = event[0]
-
-class Randomizer {
-    static func randomizeSuddenDays(for dataItem: DataItem) {
-        dataItem.SuddenDays.shuffle()
-        if dataItem.SuddenDays.count > 8 {
-            dataItem.SuddenDays = Array(dataItem.SuddenDays.prefix(8))
-        }
-    }
-    
-    static func randomizeSuddenEvent(for gamePlay: Gameplay){
-        let shuffledIds = Array(1...10).shuffled()
-        gamePlay.id = shuffledIds.first ?? 1
-        
-        
-    }
-}
-
-
-
+//let event = [
+//    Gameplay(SuddenEventTitles: ["Sudden Title"], SuddenEventDescs: ["Sudden Event Description"], SuddenPointPluses: [1], SuddenPointMinuses: [1], SuddenPointPlusesOther: [1], SuddenPointMinusesOther: [1],SuddenEventType: 1, id: 0, Used: false)
+//]
+//
+//var gameplay: Gameplay = event[0]
+//
+//class Randomizer {
+//    static func randomizeSuddenDays(for dataItem: DataItem) {
+//        dataItem.SuddenDays.shuffle()
+//        if dataItem.SuddenDays.count > 8 {
+//            dataItem.SuddenDays = Array(dataItem.SuddenDays.prefix(8))
+//        }
+//    }
+//    
+//    static func randomizeSuddenEvent(for gamePlay: Gameplay){
+//        let shuffledIds = Array(1...10).shuffled()
+//        gamePlay.id = shuffledIds.first ?? 1
+//        
+//        
+//    }
+//}
+//
+//
+//
--- Shuttle of Destiny/View/DaySummaryView.swift
+++ Shuttle of Destiny/View/DaySummaryView.swift
@@ -1,71 +1,71 @@
-import SwiftUI
-import SwiftData
-
-struct DaySummaryView: View {
-
-    @Environment(\.modelContext) private var context
-    var pointModels: DataItem
-    @Binding var alreadyChoose: Bool
-
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color(hex: "#85292B"))
-                .cornerRadius(30)
-                .frame(width: 300, height: 300)
-            
-            VStack {
-                Spacer()
-                    .frame(height: 30.0)
-                
-                Text("Day Summary")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                
-                Spacer()
-                    .frame(height: 50.0)
-                
-                Text("Training: \(pointModels.TrainingPoint)")
-                Text("Family: \(pointModels.FamilyPoint)")
-                Text("Relationship: \(pointModels.RelationPoint)")
-              
-                Spacer()
-                    .frame(height: 30.0)
-                
-                ZStack {
-                    Rectangle()
-                        .fill(Color(hex: "#FFFFFF"))
-                        .cornerRadius(30)
-                        .frame(width: 180, height: 60)
-                    
-                    Button(action: {
-                        alreadyChoose = false
-                        pointModels.Days += 1
-                        do {
-                            try context.save()
-                        } catch {
-                            print("Failed to save data item: \(error.localizedDescription)")
-                        }
-                    }) {
-                        Text("Next")
-                            .font(.title2)
-                            .foregroundColor(.red)
-                            .frame(width: 360.0, height: 60.0)
-                    }
-                }
-                
-                Spacer()
-                    .frame(height: 50.0)
-            }
-            .padding()
-            .preferredColorScheme(.dark)
-        }
-    }
-}
-
-#Preview {
-    let container = try! ModelContainer(for: DataItem.self)
-    
-    return DaySummaryView(pointModels: DataItem(), alreadyChoose: .constant(false))
-        .modelContainer(container)
-}
+//import SwiftUI
+//import SwiftData
+//
+//struct DaySummaryView: View {
+//
+//    @Environment(\.modelContext) private var context
+//    var pointModels: DataItem
+//    @Binding var alreadyChoose: Bool
+//
+//    var body: some View {
+//        ZStack {
+//            Rectangle()
+//                .fill(Color(hex: "#85292B"))
+//                .cornerRadius(30)
+//                .frame(width: 300, height: 300)
+//            
+//            VStack {
+//                Spacer()
+//                    .frame(height: 30.0)
+//                
+//                Text("Day Summary")
+//                    .font(.title)
+//                    .fontWeight(.semibold)
+//                
+//                Spacer()
+//                    .frame(height: 50.0)
+//                
+//                Text("Training: \(pointModels.TrainingPoint)")
+//                Text("Family: \(pointModels.FamilyPoint)")
+//                Text("Relationship: \(pointModels.RelationPoint)")
+//              
+//                Spacer()
+//                    .frame(height: 30.0)
+//                
+//                ZStack {
+//                    Rectangle()
+//                        .fill(Color(hex: "#FFFFFF"))
+//                        .cornerRadius(30)
+//                        .frame(width: 180, height: 60)
+//                    
+//                    Button(action: {
+//                        alreadyChoose = false
+//                        pointModels.Days += 1
+//                        do {
+//                            try context.save()
+//                        } catch {
+//                            print("Failed to save data item: \(error.localizedDescription)")
+//                        }
+//                    }) {
+//                        Text("Next")
+//                            .font(.title2)
+//                            .foregroundColor(.red)
+//                            .frame(width: 360.0, height: 60.0)
+//                    }
+//                }
+//                
+//                Spacer()
+//                    .frame(height: 50.0)
+//            }
+//            .padding()
+//            .preferredColorScheme(.dark)
+//        }
+//    }
+//}
+//
+//#Preview {
+//    let container = try! ModelContainer(for: DataItem.self)
+//    
+//    return DaySummaryView(pointModels: DataItem(), alreadyChoose: .constant(false))
+//        .modelContainer(container)
+//}
