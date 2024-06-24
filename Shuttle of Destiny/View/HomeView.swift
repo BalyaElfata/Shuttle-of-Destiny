b2b72aaf5df6f8430c99f319a65ee9df3b210f3f
Balya Elfata <balyaelfata@yahoo.com>
2024-06-24 18:16:53 +0700

    changed DataItem name to PointModel

--- Shuttle of Destiny/View/HomeView.swift
+++ Shuttle of Destiny/View/HomeView.swift
@@ -5,7 +5,7 @@ struct HomeView: View {
     
     @EnvironmentObject private var gameplay: GameplayViewModel
     @Environment(\.modelContext) private var context
-    var pointModels: DataItem
+    var pointModels: PointModel
     var gamePlay: SuddenPointModel
     @State var alreadyChoose: Bool = false
     @State var suddenDays: Bool = false
@@ -56,7 +56,7 @@ struct HomeView: View {
 }
 
 #Preview {
-    let container = try! ModelContainer(for: DataItem.self)
+    let container = try! ModelContainer(for: PointModel.self)
     let container2 = try! ModelContainer(for: SuddenPointModel.self)
     
     
@@ -64,7 +64,7 @@ struct HomeView: View {
         SuddenPointModel(SuddenEventTitles: ["Sudden Title"], SuddenEventDescs: ["Sudden Event Description"], SuddenPointPluses: [1], SuddenPointMinuses: [1], SuddenPointPlusesOther: [1], SuddenPointMinusesOther: [1],SuddenEventType: 1, id: 0, Used: false)
     ]
     
-    return HomeView(pointModels: DataItem(), gamePlay: event[0], alreadyChoose: false, suddenDays: false)
+    return HomeView(pointModels: PointModel(), gamePlay: event[0], alreadyChoose: false, suddenDays: false)
         .modelContainer(container)
         .modelContainer(container2)
         .environmentObject(GameplayViewModel())
