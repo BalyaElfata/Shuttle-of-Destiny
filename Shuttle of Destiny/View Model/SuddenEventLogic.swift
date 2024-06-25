094103c8fe1ddc1559086a8c66dd055a9a25bb42
Balya Elfata <balyaelfata@yahoo.com>
2024-06-25 16:33:45 +0700

    Fixed sudden events not generating random events

--- Shuttle of Destiny/View Model/SuddenEventLogic.swift
+++ Shuttle of Destiny/View Model/SuddenEventLogic.swift
@@ -13,7 +13,11 @@ import SwiftData
 var pointModels: PointModel = PointModel() // SwiftData Variable
 
 let events = [
-    SuddenPointModel(SuddenEventTitles: ["Sudden Title"], SuddenEventDescs: ["Sudden Event Description"], SuddenPointPluses: [1], SuddenPointMinuses: [1], SuddenPointPlusesOther: [1], SuddenPointMinusesOther: [1],SuddenEventType: 1, id: 0, Used: false)
+    SuddenPointModel(SuddenEventTitles: "Bapak kepleset", SuddenEventDescs: "Bapak lu kepleset apa yang anda harus lakukan?", SuddenPointPluses: 1, SuddenPointMinuses: 1, SuddenPointPlusesOther: 1, SuddenPointMinusesOther: 1,SuddenEventType: 3, id: 1 ,Used: false),
+    
+    SuddenPointModel(SuddenEventTitles: "Pacar ngajak jalan", SuddenEventDescs: "Ajak jalan gak?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2,SuddenEventType: 1, id: 2 ,Used: false),
+    
+    SuddenPointModel(SuddenEventTitles: "Hans ajak gym", SuddenEventDescs: "Mau ikut ato enggak?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2, SuddenEventType: 2, id: 3, Used: false)
 ]
 
 var gameplay: SuddenPointModel = events[0]
@@ -27,7 +31,7 @@ class Randomizer {
     }
     
     static func randomizeSuddenEvent(for gamePlay: [SuddenPointModel]) -> SuddenPointModel {
-        let shuffledIds = Array(1...10).shuffled()
+        let shuffledIds = Array(1...3).shuffled()
         
         guard let filteredEvent = gamePlay.filter ({ event in
             event.id == shuffledIds.first
