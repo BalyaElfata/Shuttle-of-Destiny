8337f4f1ed899645a87b5f0356278f3c1f00ca79
Balya Elfata <balyaelfata@yahoo.com>
2024-06-25 21:14:36 +0700

    Changed order of sudden events and adjusted function argument names

--- Shuttle of Destiny/View Model/SuddenEventLogic.swift
+++ Shuttle of Destiny/View Model/SuddenEventLogic.swift
@@ -13,27 +13,27 @@ import SwiftData
 var pointModels: PointModel = PointModel() // SwiftData Variable
 
 let events = [
-    SuddenPointModel(SuddenEventTitles: "Bapak kepleset", SuddenEventDescs: "Bapak lu kepleset apa yang anda harus lakukan?", SuddenPointPluses: 1, SuddenPointMinuses: 1, SuddenPointPlusesOther: 1, SuddenPointMinusesOther: 1,SuddenEventType: 3, id: 1 ,Used: false),
+    SuddenPointModel(SuddenEventTitles: "Pacar ngajak jalan", SuddenEventDescs: "Ajak jalan gak?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2,SuddenEventType: 1, id: 1, Used: false),
     
-    SuddenPointModel(SuddenEventTitles: "Pacar ngajak jalan", SuddenEventDescs: "Ajak jalan gak?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2,SuddenEventType: 1, id: 2 ,Used: false),
+    SuddenPointModel(SuddenEventTitles: "Hans ajak gym", SuddenEventDescs: "Mau ikut ato enggak?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2, SuddenEventType: 2, id: 2, Used: false),
     
-    SuddenPointModel(SuddenEventTitles: "Hans ajak gym", SuddenEventDescs: "Mau ikut ato enggak?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2, SuddenEventType: 2, id: 3, Used: false)
+    SuddenPointModel(SuddenEventTitles: "Bapak kepleset", SuddenEventDescs: "Bapak lu kepleset apa yang anda harus lakukan?", SuddenPointPluses: 2, SuddenPointMinuses: 2, SuddenPointPlusesOther: 2, SuddenPointMinusesOther: 2,SuddenEventType: 3, id: 3, Used: false),
 ]
 
 var gameplay: SuddenPointModel = events[0]
 
 class Randomizer {
-    static func randomizeSuddenDays(for dataItem: PointModel) {
-        dataItem.SuddenDays.shuffle()
-        if dataItem.SuddenDays.count > 8 {
-            dataItem.SuddenDays = Array(dataItem.SuddenDays.prefix(8))
+    static func randomizeSuddenDays(for pointModel: PointModel) {
+        pointModel.SuddenDays.shuffle()
+        if pointModel.SuddenDays.count > 8 {
+            pointModel.SuddenDays = Array(pointModel.SuddenDays.prefix(8))
         }
     }
     
-    static func randomizeSuddenEvent(for gamePlay: [SuddenPointModel]) -> SuddenPointModel {
+    static func randomizeSuddenEvent(for suddenPointModel: [SuddenPointModel]) -> SuddenPointModel {
         let shuffledIds = Array(1...3).shuffled()
         
-        guard let filteredEvent = gamePlay.filter ({ event in
+        guard let filteredEvent = suddenPointModel.filter ({ event in
             event.id == shuffledIds.first
         }).first else { return events[0] }
         
