6d726d36fd23de2c2acff40aff24453ba69792c1
Lonard Steven <lonardnew@gmail.com>
2024-06-26 13:50:35 +0700

    Removed unneeded views & adding hospital room background

---
+++ Shuttle of Destiny/View/WaitingView.swift
@@ -0,0 +1,18 @@
+//
+//  WaitingView.swift
+//  Shuttle of Destiny
+//
+//  Created by Lonard Steven on 26/06/24.
+//
+
+import SwiftUI
+
+struct WaitingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaitingView()
+}
